5d7466f03989a74a43eed3e2f2d736981959073c
Mike Lambert <mikelambert@mikebook.local>
2014-06-20 17:30:08 -0400

    Adding ability to download all Podcasts

--- src/radiolab/RadiolabScraper.java
+++ src/radiolab/RadiolabScraper.java
@@ -1,8 +1,9 @@
 package radiolab;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -20,16 +21,136 @@ public class RadiolabScraper {
 	
 	public void run() {
 		
-		int[] getTheseSeason = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-		
-		
+		// seasons
+		int[] getTheseSeason = {};
 		for (int getThis : getTheseSeason) {
+			this.getRadioLabSeason(getThis);	
+		}
+		
+		// podcasts
+		this.getRadioLabPodcasts();
+
+		
+
+		
+	}
+	
+	protected void getRadioLabPodcasts() {
+		
+		try {
+			Document doc = Jsoup.connect("http://www.radiolab.org/series/podcasts/").get();
 			
-			this.getRadioLabSeason(getThis);
+			// find how many pages there are
+			int podcastPages = Integer.parseInt(doc.select(".pagefooter-link").last().text());
+
+			// scrape pages n to 2
+			for(int n = podcastPages; n > 1; n--){
+				Document page = Jsoup.connect("http://www.radiolab.org/series/podcasts/" + n).get();
+				this.scrapePodcastListingPage(page);
+			}
+			// scrape page 1
+			this.scrapePodcastListingPage(doc);
+
+			System.out.println("Done with podcasts.");	
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * http://www.radiolab.org/series/podcasts/
+	 * @param doc
+	 */
+	protected void scrapePodcastListingPage(Document doc) {
+		
+		try {
+
+			Elements links = doc.select(".series-item h2.title a");
 			
+			for (Element l : links){
+				String href = l.attr("href");
+				Document detailsPage = Jsoup.connect(href).get();
+				this.scrapeStory(detailsPage);
+			}
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected void scrapeStory(Document detailsPage){
+
+		try {
+			// skip this is it's a season episode
+			String se = detailsPage.select(".seanum-epnum").text();
+			if (!se.equals("")){
+				System.out.println("Skipping, this is an episode: " + detailsPage.baseUri());
+				return;
+			}
+			
+			String title = detailsPage.select(".article-full .title").first().text();
+			String date = this.parseDate(detailsPage.select(".article-full .date").first().text());
+			
+			String filepath = "Downloaded";
+			filepath += File.separator;
+			filepath += "Podcasts";
+			filepath += File.separator;			
+			
+			
+			String filename = date;
+			filename += " - ";
+			filename += title;
+			filename += ".mp3";
+					
+			Element player = detailsPage.select(".inline_audioplayer_wrapper").first();
+			if (player == null){
+				System.out.println("Skipping, no player on this page: " + title);
+				return;
+			}
+			
+			String xmlDocUrl = player.select(".player_element").first().attr("data-url");
+			Document xmlDoc = Jsoup.connect(xmlDocUrl).get();
+			String downloadLink = xmlDoc.select("location").text();
+			
+			URL downloadUrl = new URL(downloadLink);
+			
+			File saveFile = new File(filepath+filename);
+			
+			if (saveFile.exists()){
+				System.out.println("Skipping, file already exists: " + filename);
+				return;
+			}
+			System.out.println("Downloading " + filename + " ...");	
+			org.apache.commons.io.FileUtils.copyURLToFile(downloadUrl, saveFile);
+			
+		
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		
 	}
+	
+	protected String parseDate(String dateString){
+		
+		try {
+			SimpleDateFormat parseFormat = new SimpleDateFormat("EEEE, MMMM d, yyyy");
+			Date date = parseFormat.parse(dateString);
+			
+			SimpleDateFormat toFormat = new SimpleDateFormat("yyyy-MM-dd");
+			String formattedDate = toFormat.format(date);
+			
+			return formattedDate;
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			return "";
+		}
+		
+	}
+	
+
 	
 	protected void getRadioLabSeason(int season){
 		
@@ -67,7 +188,7 @@ public class RadiolabScraper {
 				}
 				
 			}
-//			System.out.println(i + " episodes");
+			System.out.println("Done with " + i + " episodes");
 			
 			
 		} catch (Exception e) {
@@ -104,7 +225,7 @@ public class RadiolabScraper {
 			
 			filename = this.getValidFileName(filename);
 			
-			// download file
+			// download file. find the mp3 link in this trickey xml file for the player
 			String xmlDocUrl = doc.select(".inline_audioplayer_wrapper").first().select(".player_element").first().attr("data-url");
 			Document xmlDoc = Jsoup.connect(xmlDocUrl).get();
 			String downloadLink = xmlDoc.select("location").text();
